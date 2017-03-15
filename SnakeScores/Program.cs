74baed9152637d0f1c7b6d3ab9f78b08f8464588
DESKTOP-68FETGO\Samuel <samuelbenais@outlook.com>
2017-03-16 00:57:37 +0100

    w<dcw

---
+++ SnakeScores/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeScores
+{
+    class Program
+    {
+        #region Constants
+
+        private const string SCORES_FILENAME = @"scores.xml";
+
+        #endregion Constants
+
+        #region Entry point
+
+        static void Main(string[] args)
+        {
+            // Deserialization from disk
+            Score.LoadData(SCORES_FILENAME);
+
+            // Enter the students
+            for (;;)
+            {
+                Console.WriteLine("Enter a student name (empty to finish)");
+                String studentName = Console.ReadLine();
+                if (String.IsNullOrEmpty(studentName)) break;
+
+                Console.WriteLine("Enter a student firstname (empty to finish)");
+                String studentFirstName = Console.ReadLine();
+                if (String.IsNullOrEmpty(studentName)) break;
+
+                Console.WriteLine("Enter the student mark");
+                Int32 studentMark = Int32.Parse(Console.ReadLine());
+
+                // Add student in the array
+                Score.RegisterScore(studentName, studentFirstName, studentMark);
+            }
+
+            // Dump the current student list
+            Score.ConsoleDump();
+
+            // Serialization on disk
+            Score.SaveData(SCORES_FILENAME);
+
+            Console.ReadLine();
+        }
+
+        #endregion Entry point
+    }
+}
---
+++ SnakeScores/Score.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SnakeScores
+{
+        [Serializable]
+        public class Score
+        {
+
+            private static List<Score> scores = new List<Score>();
+
+
+            private static XmlSerializer xs = new XmlSerializer(typeof(List<Score>));
+
+
+            public static void LoadData(String filename)
+            {
+               Debug.WriteLine("[LIBINFO] Reading file " + filename);
+
+               scores = null;
+
+                try
+                {
+                    if (File.Exists(filename))
+                    {
+                        using (XmlReader xr = XmlReader.Create(filename))
+                        {
+                            scores = (List<Score>)xs.Deserialize(xr);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine("[LIBERROR] Error reading the file " + filename);
+                    Debug.WriteLine("           " + e.ToString());
+                }
+                finally
+                {
+                    if (scores == null)
+                    {
+                        scores = new List<Score>();
+                    }
+                }
+            }
+
+            public static void SaveData(String filename)
+            {
+                Debug.WriteLine("[LIBINFO] Writing file " + filename);
+
+                try
+                {
+                    using (XmlWriter xw = XmlWriter.Create(filename))
+                    {
+                        xs.Serialize(xw, scores);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine("[LIBERROR] Error writing the file " + filename);
+                    Debug.WriteLine("           " + e.ToString());
+                }
+            }
+
+            public static void ResetData()
+            {
+                Debug.WriteLine("[LIBINFO] Reset data context");
+                scores = new List<Score>();
+            }
+
+            public static void DebugDump()
+            {
+                foreach (Score student in scores)
+                {
+                    Debug.WriteLine(student.ToString());
+                }
+            }
+
+            public static void ConsoleDump()
+            {
+                foreach (Score student in scores)
+                {
+                    Console.WriteLine(student.ToString());
+                }
+            }
+
+            public static Score RegisterScore(Score NewScore)
+            {
+                scores.Add(NewScore);
+                return NewScore;
+            }
+
+            public static Score RegisterScore(String firstname, String lastname, int grade)
+            {
+                return RegisterScore(new Score(firstname, lastname, grade));
+            }
+
+            public static List<Score> Scores
+            {
+                get { return scores; }
+            }
+
+            public String firstname { get; /*private*/ set; }
+            public String lastname { get; /*private*/ set; }
+            public Int32 grade { get; /*private*/ set; } 
+
+            public Score()
+            {
+                this.grade = 0;
+                this.firstname = null;
+                this.lastname = null;
+            }
+
+            public Score(String firstname, String lastname, int grade)
+            {
+                this.grade = grade;
+                this.firstname = firstname;
+                this.lastname = lastname;
+            }
+
+        }
+    
+
+}
